ab24293e13bf3127255f012d1659adeaf091e6cb
anh xbro <anh.bigbrother@gmail.com>
2025-02-06 00:23:50 +0700

    fix rootCmd==<nil>

--- cmd/root.go
+++ cmd/root.go
@@ -13,31 +13,25 @@ import (
 	"github.com/AnhBigBrother/todo/data"
 )
 
-var (
-	todos   data.Todos
-	store   *data.Storage[data.Todos]
-	rootCmd *cobra.Command
-)
+var rootCmd = &cobra.Command{
+	Use:   "todo",
+	Short: "todo-list cli",
+	Long:  "simple todo-list cli app",
+}
+
+var todos = data.Todos{}
+var homeDir, _ = os.UserHomeDir()
+var store = data.NewStorage[data.Todos](fmt.Sprintf("%s/todo_data.json", homeDir))
 
 func Execute() {
-	todos = data.Todos{}
-
-	homeDir, _ := os.UserHomeDir()
-	store = data.NewStorage[data.Todos](fmt.Sprintf("%s/todo_data.json", homeDir))
-
-	rootCmd = &cobra.Command{
-		Use:   "todo",
-		Short: "todo-list cli",
-		Long:  "simple todo-list cli app",
-	}
-
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
+		fmt.Println(err.Error())
 	}
 	err = store.Save(todos)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println(err.Error())
 	}
 }
 
